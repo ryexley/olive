772b942ebe0ecbbb33cd8c8fce801b356e124961
Bob Yexley <bob@yexley.net>
2020-11-30 10:15:15 -0500

    refactor: moved everything (except "public") into a src folder

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require("path")
 module.exports = {
   sassOptions: {
     includePaths: [
-      path.join(__dirname, "styles"),
+      path.join(__dirname, "src/styles"),
       path.join(__dirname, "node_modules")
     ]
   }
